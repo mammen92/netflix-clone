03964a954e194f73fdfe1da1e2837bddb3702827
Mammen Paul <mammen123@gmail.com>
2023-08-09 20:23:12 +0530

    Updates

---
+++ precache-manifest.3b445174e75226b5818b679396b5ccc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea26cbe2c2225171e9fa",
+    "url": "/netflix-clone/static/css/main.caae16e8.chunk.css"
+  },
+  {
+    "revision": "ea26cbe2c2225171e9fa",
+    "url": "/netflix-clone/static/js/main.ea26cbe2.chunk.js"
+  },
+  {
+    "revision": "75e7f137b94d8322f788",
+    "url": "/netflix-clone/static/js/1.75e7f137.chunk.js"
+  },
+  {
+    "revision": "0b892cc98d3bb0874451",
+    "url": "/netflix-clone/static/js/runtime~main.0b892cc9.js"
+  },
+  {
+    "revision": "042eb54de782e7bd46c69b5dfdee7756",
+    "url": "/netflix-clone/index.html"
+  }
+];
